c088146500df1053ecda300b0594531a55ead6eb
TayfunF <firtinatayfun08@gmail.com>
2022-05-15 19:41:11 +0300

    Mapping Added

---
+++ TayfunsBlog.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TayfunsBlog.Core.Dtos;
+using TayfunsBlog.Core.Models;
+
+namespace TayfunsBlog.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<AboutDto, About>().ReverseMap();
+            CreateMap<CategoryDto, Category>().ReverseMap();
+            CreateMap<CommentDto, Comment>().ReverseMap();
+            CreateMap<PostDto, Post>().ReverseMap();
+            CreateMap<WriterDto, Writer>().ReverseMap();
+        }
+    }
+}
