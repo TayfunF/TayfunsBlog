d94262f520f4333d0496e9034e3062ce926deca5
TayfunF <firtinatayfun08@gmail.com>
2022-05-15 20:14:06 +0300

    Template, Partial Page Created

---
+++ TayfunsBlog.Web/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TayfunsBlog.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
