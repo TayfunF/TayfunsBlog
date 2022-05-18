90e02c92d72690f1d285ab2632c148fc5ee2b9c1
TayfunF <firtinatayfun08@gmail.com>
2022-05-18 15:36:30 +0300

    Index Updated

--- TayfunsBlog.Web/Controllers/DefaultController.cs
+++ TayfunsBlog.Web/Controllers/DefaultController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TayfunsBlog.Core.Dtos;
+using TayfunsBlog.Core.Services;
 
 namespace TayfunsBlog.Web.Controllers
 {
     public class DefaultController : Controller
     {
-        public IActionResult Index()
+        private readonly IPostService _postService;
+        private readonly IMapper _mapper;
+
+        public DefaultController(IPostService postService, IMapper mapper)
         {
-            return View();
+            _postService = postService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var Posts = await _postService.GetAllAsync();
+            var PostsDtos = _mapper.Map<List<PostDto>>(Posts);
+            var PostsDtosOrderBy = PostsDtos.OrderByDescending(x => x.Id).ToList();
+
+            return View(PostsDtosOrderBy);
         }
     }
 }
